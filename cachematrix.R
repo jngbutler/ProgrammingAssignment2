b53570bb94c83b589c92649a6dcd94af5cbe70ef
jngbutler <jngbutler@gmail.com>
2014-04-26 18:46:49 -0400

    peer assignment

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,61 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Overall Description: The purpose of the following pair of functions is to 
+## cache the inverse of a matrix.  Having a cached inverse avoids costly
+## computation (having to compute the inverse of the matrix repeatedly).
 
-## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
+## makeCacheMatrix is a function that creates a list of 4 objects:
+## 1) set() is a function to set the contents of the matrix
+## 2) get() is a function to retrieve the contents of the matrix
+## 3) setInverse() is a function to set the contents of the inverted matrix
+## 4) getInverse() is a function to retrieve the contents of the inverted matrix
 
+## The purpose of this first function makeCacheMatrix is to store (cache) 
+## the contents of both the original matrix and its inverse.
+
+makeCacheMatrix <- function(x=numeric()) {
+  m <- NULL
+  
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  
+  get <- function() x
+  setInverse <- function(inverse) m <<- inverse
+  getInverse <- function() m
+  
+  list(set=set, get=get,
+       setInverse=setInverse,
+       getInverse=getInverse
+  )
 }
 
 
-## Write a short comment describing this function
+## The purpose of cacheSolve is that it is a function that returns an inverse of a matrix.
+## It does this in one of the two following ways:
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+## 1) IF the inverted matrix has already been cached (this function has already seen 
+## the matrix returned from cacheMakeMatrix and prevously cached its inverse) AND
+## the contents of the matrix has not changed, THEN it simply retrieves the cached
+## inverted matrix.
+
+## 2) Otherwise, it takes the matrix returned from makeCacheMatrix and calculates 
+## its inverse and caches it.
+
+makeCacheMatrix <- function(x=numeric()) {
+  m <- NULL
+  
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  
+  get <- function() x
+  setInverse <- function(inverse) m <<- inverse
+  getInverse <- function() m
+  
+  list(set=set, get=get,
+       setInverse=setInverse,
+       getInverse=getInverse
+  )
 }
